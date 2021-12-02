3f58f5335e42e050308a748bb5ee4ab81cf73b83
sanjunabandi <89703392+sanjunabandi@users.noreply.github.com>
2021-12-02 22:34:30 +0530

    Checkpoint12 (#3)
    * Checkpoint 12

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,10 +1,13 @@
-var calculateTotalPrice = function () {
-  let form = document.getElementById("form");
-  let adult = document.getElementById("adult").value;
-  let name = document.getElementById("name").value;
-  let fromDate = document.getElementById("fromDate").valueAsNumber;
-  let toDate = document.getElementById("toDate").valueAsNumber;
-  let totalPrice = document.getElementById("price");
+let form1 = document.getElementById("form");
+let adult, nameOfCus, fromDate, toDate, totalPrice, priceToDisplay;
+
+//Calculate total price function
+const calculateTotalPrice = function () {
+  adult = document.getElementById("adult").value;
+  nameOfCus = document.getElementById("name").value;
+  fromDate = document.getElementById("fromDate").valueAsNumber;
+  toDate = document.getElementById("toDate").valueAsNumber;
+  totalPrice = document.getElementById("price");
 
   let price = 1000;
   let toDateEl = document.getElementById("toDate");
@@ -13,15 +16,171 @@ var calculateTotalPrice = function () {
   toDateEl.min = fromDateEl.value;
   if (
     adult >= 1 &&
-    name != "" &&
+    nameOfCus != "" &&
     fromDate > 0 &&
     toDate > 0 &&
     toDate > fromDate
   ) {
     let days = (toDate - fromDate) / 86400000;
-    let priceToDisplay = price * adult * days;
+    priceToDisplay = price * adult * days;
     totalPrice.value = "Rs. " + priceToDisplay;
   }
 };
 
-form.addEventListener("input", calculateTotalPrice);
+form1.addEventListener("input", calculateTotalPrice);
+
+//////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS
+// function rating
+const rating = async function (rating) {
+  let ratingVal = Number.parseFloat(rating) * 10;
+  let rem = ratingVal % 10;
+  let quotient = Math.floor(ratingVal / 10);
+  let ratingHtml = "";
+  let html = `<span class="fa fa-star checked"></span>`;
+  for (let i = 0; i < quotient; i++) {
+    ratingHtml = ratingHtml + html;
+  }
+  if (rem !== 0) {
+    ratingHtml += `<span class="fa fa-star-half-o checked"></span>`;
+  }
+  return ratingHtml;
+};
+
+//function amenities
+const amenities = async function (amenitiesObj) {
+  let html = "";
+  let key = "";
+  let replacedKey = "";
+  let finalKey = "";
+  for (let y = 0; y < 10; y++) {
+    key = amenitiesObj[y].key;
+    replacedKey = key.replaceAll("_", " ");
+    finalKey = replacedKey[0].toUpperCase() + replacedKey.slice(1);
+    html = html + `<li>${finalKey}</li>`;
+  }
+  return html;
+};
+
+////////////////////////////////////API FOR HOTEL DETAILS////////////////////////////////////
+let hotelDescription = document.getElementById("hotel-description-para");
+let hotelName = document.getElementById("hotel-name");
+let ratings = document.getElementById("ratings-detail");
+let amenitiesUl = document.getElementById("amenities-ul");
+let resultObjArr;
+
+let url = window.location.href;
+const hotelId = url.split("?")[1].split("=")[1];
+
+const data = null;
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = false;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    // console.log(JSON.parse(this.responseText));
+    resultObjArr = JSON.parse(this.responseText);
+    hotelName.textContent = resultObjArr.data[0].name;
+    hotelDescription.textContent = resultObjArr.data[0].description;
+    rating(resultObjArr.data[0].rating).then(
+      (html) => (ratings.innerHTML = html)
+    );
+    amenities(resultObjArr.data[0].amenities).then(
+      (amenitiesHtml) => (amenitiesUl.innerHTML = amenitiesHtml)
+    );
+  }
+});
+
+xhr.open(
+  "GET",
+  `https://travel-advisor.p.rapidapi.com/hotels/get-details?location_id=${hotelId}`
+);
+xhr.setRequestHeader("x-rapidapi-host", "travel-advisor.p.rapidapi.com");
+xhr.setRequestHeader(
+  "x-rapidapi-key",
+  "999a19f357msh98c6dffba35d2cdp1595abjsndc394e46ecbc"
+);
+
+xhr.send(data);
+
+///////////////////////////////////// API FOR PhotoLists///////////////////////////////////
+const carousel = function (imagesObj) {
+  return `<div class="carousel-item active">
+  <img
+    class="carousel-image"
+    class="d-block w-100"
+    src="${imagesObj[0].images.original.url}"
+    alt="First slide"
+  />
+</div>
+<div class="carousel-item">
+  <img
+    class="carousel-image"
+    class="d-block w-100"
+    src="${imagesObj[1].images.original.url}"
+    alt="Second slide"
+  />
+</div>
+<div class="carousel-item">
+  <img
+    class="carousel-image"
+    class="d-block w-100"
+    src="${imagesObj[2].images.original.url}"
+    alt="Third slide"
+  />
+</div>
+<div class="carousel-item">
+  <img
+    class="carousel-image"
+    class="d-block w-100"
+    src="${imagesObj[3].images.original.url}"
+    alt="Fourth slide"
+  />
+</div>
+<div class="carousel-item">
+  <img
+    class="carousel-image"
+    class="d-block w-100"
+    src="${imagesObj[4].images.original.url}"
+    alt="Fifth slide"
+  />
+</div>`;
+};
+let carouselInner = document.getElementsByClassName("carousel-inner")[0];
+
+const data1 = null;
+const xhr1 = new XMLHttpRequest();
+xhr1.withCredentials = false;
+
+xhr1.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    let resObj = JSON.parse(this.responseText).data;
+    let carouselHtml = carousel(resObj);
+    carouselInner.innerHTML = carouselHtml;
+  }
+});
+
+xhr1.open(
+  "GET",
+  `https://travel-advisor.p.rapidapi.com/photos/list?location_id=${hotelId}`
+);
+xhr1.setRequestHeader("x-rapidapi-host", "travel-advisor.p.rapidapi.com");
+xhr1.setRequestHeader(
+  "x-rapidapi-key",
+  "999a19f357msh98c6dffba35d2cdp1595abjsndc394e46ecbc"
+);
+
+xhr1.send(data1);
+
+//////////////////////////////////////////////////////////////////
+// const formBtn = document.getElementById("book-now");
+form1.addEventListener("submit", function (event) {
+  event.preventDefault();
+  window.location.href = `payment.html?hotel_id=${hotelId}&adults=${
+    document.getElementById("adult").value
+  }&name=${document.getElementById("name").value}&fromDate=${
+    document.getElementById("fromDate").value
+  }&toDate=${
+    document.getElementById("toDate").value
+  }&totalPrice=${priceToDisplay}`;
+});
---
+++ scripts/list.js
@@ -0,0 +1,81 @@
+let url = window.location.href;
+let cityName = url.split("?")[1].split("=")[1];
+
+////////////////////////////////////////////////API/////////////////////////////////////////////////
+//This will give hotel list from a particular city
+const data = null;
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = false;
+let resultObjArr = new Array();
+let filteredResultObjArr = new Array();
+
+function filterHotels(hotels) {
+  return !(hotels.rating === undefined && hotels.address === undefined);
+}
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    let arr = JSON.parse(this.responseText).data;
+    for (var i = 0; i < arr.length; i++) {
+      resultObjArr.push(arr[i].result_object);
+    }
+    filteredResultObjArr = resultObjArr.filter(filterHotels);
+    hotelDetailsTemplate(filteredResultObjArr);
+  }
+});
+
+xhr.open(
+  "GET",
+  `https://travel-advisor.p.rapidapi.com/locations/search?query=${cityName}`
+);
+xhr.setRequestHeader("x-rapidapi-host", "travel-advisor.p.rapidapi.com");
+xhr.setRequestHeader(
+  "x-rapidapi-key",
+  "999a19f357msh98c6dffba35d2cdp1595abjsndc394e46ecbc"
+);
+
+xhr.send(data);
+
+let listView = document.getElementById("list-view");
+let hotelContainer;
+let clickedHotelName;
+let hotelId;
+
+const hotelDetailsTemplate = function (hotelsArray) {
+  //   console.log(hotelsArray);
+  const str = hotelsArray
+    .map((object) => {
+      return `<a href="#"><section class="hotel-image-container">
+    <img
+      src="${object.photo.images.medium.url}"
+      class="hotel-image"
+      alt="${object.name}"
+    />
+    <div class="hotel-details">
+      <h3>${object.name}</h3>
+      <p>${object.rating}<span class="fa fa-star checked"></span></p>
+      <p>${object.address}</p>
+    </div>
+    </section></a>`;
+    })
+    .join("");
+  listView.innerHTML = str;
+  hotelContainer = document.getElementsByClassName("hotel-image-container");
+
+  for (var i = 0; i < hotelContainer.length; i++) {
+    hotelContainer[i].addEventListener("click", function (event) {
+      event.preventDefault();
+
+      clickedHotelName = event.target.closest(".hotel-image-container")
+        .childNodes[3].childNodes[1].textContent;
+      for (let a = 0; a < filteredResultObjArr.length; a++) {
+        // console.log(filteredResultObjArr[a].name);
+        if (filteredResultObjArr[a].name === clickedHotelName) {
+          hotelId = filteredResultObjArr[a].location_id;
+          break;
+        }
+      }
+      window.location.href = `detail.html?id=${hotelId}`;
+    });
+  }
+};
