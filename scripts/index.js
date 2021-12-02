5c2540eeddf5b555d9d06a32a5af23bd145ead6d
sanjunabandi <89703392+sanjunabandi@users.noreply.github.com>
2021-12-03 00:19:13 +0530

    Checkpoint12 cleanup (#4)
    * Checkpoint12

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,22 @@
-//view more button functionality
 let cityCardsTwo = document.getElementById("city-cards-2");
 let viewMoreButton = document.getElementById("view-more-btn");
-
-viewMoreButton.addEventListener("click", function () {
-  if (cityCardsTwo.dataset.value === "view-less") {
-    cityCardsTwo.dataset.value = "view-more";
-    cityCardsTwo.style.display = "none";
-    viewMoreButton.innerHTML = "View More";
-  } else {
-    cityCardsTwo.dataset.value = "view-less";
-    cityCardsTwo.style.display = "flex";
-    viewMoreButton.innerHTML = "View Less";
-  }
+viewMoreButton.addEventListener("click", function() {
+	if(cityCardsTwo.dataset.value === "view-less") {
+		cityCardsTwo.dataset.value = "view-more";
+		cityCardsTwo.style.display = "none";
+		viewMoreButton.innerHTML = "View More";
+	} else {
+		cityCardsTwo.dataset.value = "view-less";
+		cityCardsTwo.style.display = "flex";
+		viewMoreButton.innerHTML = "View Less";
+	}
 });
-
 var cityImgDiv = document.querySelectorAll(".city-image");
-
-//This will pass on the city you clicked
-for (var i = 0; i < cityImgDiv.length; i++) {
-  cityImgDiv[i].addEventListener("click", function (event) {
-    event.preventDefault();
-    let cityName = event.target.closest(".city-image").getAttribute("id");
-    window.location.href = `list.html?city=${cityName}`;
-  });
+for(var i = 0; i < cityImgDiv.length; i++) {
+	cityImgDiv[i].addEventListener("click", function(event) {
+		event.preventDefault();
+		let cityName = event.target.closest(".city-image").getAttribute("id");
+		window.location.href = `list.html?city=${cityName}`;
+	});
 }
+
